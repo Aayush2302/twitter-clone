c7710e17efc7ce00a03a27d5dbcf8e9aad271dd4
Aayush <aayushjivanifch01@gmail.com>
2024-05-25 17:12:36 +0530

    1 - post routes and controllers added

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import Post from "./post.model.js";
 
 const userSchema = new mongoose.Schema(
   {
@@ -16,6 +17,13 @@ const userSchema = new mongoose.Schema(
     coverImg: { type: String, default: "" },
     bio: { type: String, default: "" },
     link: { type: String, default: "" },
+    likedPosts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+        default: [],
+      },
+    ],
   },
   { timestamps: true }
 );
--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
+
+//routes
+import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
 dotenv.config();
 
 cloudinary.config({
@@ -20,6 +23,7 @@ app.use(express.urlencoded({ extended: true })); // to parse form data
 app.use(cookieParser()); // to parse cookies
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
   connectMongoDB();
