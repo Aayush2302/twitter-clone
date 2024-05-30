82b588f3363cc969b376480ba3bf904aebaf0f2a
Aayush <aayushjivanifch01@gmail.com>
2024-05-30 09:39:42 +0530

    3- follow in Right Panel done

--- Backend/server.js
+++ Backend/server.js
@@ -19,7 +19,7 @@ cloudinary.config({
 const app = express();
 
 // middlewares
-app.use(express.json()); //to parse req.body
+app.use(express.json({ limit: "5mb" })); //to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse form data
 app.use(cookieParser()); // to parse cookies
 app.use("/api/auth", authRoutes);
